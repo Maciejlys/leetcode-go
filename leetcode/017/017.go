e674f55146942b31642e7d3e529439c861a6ebd2
maciej.lyszczarz <maciej.lyszczarz@jamf.com>
2024-09-04 23:10:10 +0200

    fix

--- leetcode/017/017.go
+++ leetcode/017/017.go
@@ -5,7 +5,7 @@ var letters = map[string][]string{
 	"3": []string{"d", "e", "f"},
 	"4": []string{"g", "h", "i"},
 	"5": []string{"j", "k", "l"},
-	"6": []string{"n", "n", "o"},
+	"6": []string{"m", "n", "o"},
 	"7": []string{"p", "q", "r", "s"},
 	"8": []string{"t", "u", "v"},
 	"9": []string{"w", "x", "y", "z"},
